47f6dbbfa55f96d8331525973569f9ca06ebd8a2
julianaMonteiro <juliana47monteiro@gmail.com>
2025-02-18 13:56:15 -0300

    consumindo API Login e Cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Footer from './components/footer/Footer'
-import Navbar from './components/navbar/Navbar'
-import Home from './pages/home/Home'
-import Login from './pages/login/Login'
-import Cadastro from './pages/cadastro/Cadastro'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Cadastro from './pages/cadastro/Cadastro';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
 
 function App() {
+
   return (
     <>
+      <AuthProvider>
         <BrowserRouter>
-          <Navbar />
+          <Navbar/>
           <div className="min-h-[80vh]">
             <Routes>
               <Route path="/" element={<Login />} />
@@ -19,10 +22,11 @@ function App() {
               <Route path="/login" element={<Login />} />
             </Routes>
           </div>
-          <Footer />
+          <Footer/>
         </BrowserRouter>
+      </AuthProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
