98debe4c9537d650b8fa110b95253d1b6581d1b4
julianaMonteiro <juliana47monteiro@gmail.com>
2025-02-19 14:56:37 -0300

    implementando CRUD

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import Login from './pages/login/Login';
 import Cadastro from './pages/cadastro/Cadastro';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
+import ListaTemas from './components/temas/listatemas/ListaTemas';
+import FormTema from './components/temas/formtema/FormTema';
+import DeletarTema from './components/temas/deletartema/DeletarTema';
+import ListaPostagens from './components/postagens/listapostagens/ListaPostagens';
 
 function App() {
 
@@ -20,6 +24,11 @@ function App() {
               <Route path="/home" element={<Home />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastrartema" element={<FormTema />} />
+              <Route path="/editartema/:id" element={<FormTema />} />
+              <Route path="/deletartema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
             </Routes>
           </div>
           <Footer/>
